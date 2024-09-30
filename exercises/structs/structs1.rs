9463e361116d63d8e37d4fa4400c0b01de1c15e2
oslab <2827886640@qq.com>
2024-09-30 22:26:41 +0800

    solve structs1

--- exercises/structs/structs1.rs
+++ exercises/structs/structs1.rs
@@ -1,3 +1,10 @@
+// structs1.rs
+//
+// Address all the TODOs to make the tests pass!
+//
+// Execute `rustlings hint structs1` or use the `hint` watch subcommand for a
+// hint.
+
 #[cfg(test)]
 mod tests {
     use super::*;
